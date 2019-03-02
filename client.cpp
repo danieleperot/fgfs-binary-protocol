8c581acd17a73931c43eb0159df6770e8cb02ae0
Daniele Perot <perotdaniele@gmail.com>
2019-03-02 19:45:05 +0100

    English text for everybody!

--- client.cpp
+++ client.cpp
@@ -15,50 +15,49 @@ int main() {
     UDPConnect* udp_con = new UDPConnect("127.0.0.1", 10540);
 
     while (true) {
-        cout << "\n[a]ileron\n[e]levator\n[r]udder\n[t]hrottle\n[m]agnetos\nmi[x]ture\n[s]end\n[0] reset\n[q]uit\nInserisci un dato:\t";
+        cout << "\n[a]ileron\n[e]levator\n[r]udder\n[t]hrottle\n[m]agnetos\nmi[x]ture\n[0] reset\n[q]uit\nChoose an option:\t";
         cin >> choice;
 
         if (choice == 'a') {
-            cout << "Valore aileron:\t";
+            cout << "Aileron (-1.0 to +1.0):\t";
             cin >> doubleval;
             // udp_con->send(&doubleval, sizeof(doubleval));
             flightgear->setAlieron(doubleval)->send();
         }
         if (choice == 'e') {
-            cout << "Valore elevator:\t";
+            cout << "Elevator (-1.0 to +1.0):\t";
             cin >> doubleval;
             flightgear->setElevator(doubleval)->send();
         }
         if (choice == 'r') {
-            cout << "Valore rudder:\t";
+            cout << "Rudder (-1.0 to +1.0):\t";
             cin >> doubleval;
             flightgear->setRudder(doubleval)->send();
         }
         if (choice == 't') {
-            cout << "Valore throttle:\t";
+            cout << "Throttle (0.0 to +1.0):\t";
             cin >> doubleval;
             flightgear->setThrottle(doubleval)->send();
         }
         if (choice == 'x') {
-            cout << "Valore mixture:\t";
+            cout << "Mixture (0.0 to 1.0):\t";
             cin >> doubleval;
             flightgear->setMixture(doubleval)->send();
         }
         if (choice == 'm') {
-            cout << "Valore magnetos:\t";
+            cout << "Magnetos (0 to 4):\t";
             cin >> intval;
             flightgear->setMagnetos(intval)->send();
         }
-        if (choice == 's') {
-            cout << "Invio in corso...\n";
-            flightgear->send();
-        }
         if (choice == '0') {
-            cout << "Reset in corso...\n";
+            cout << "Reset...\n";
             flightgear->resetData()->send();
         }
         if (choice == 'q') {
             exit(0);
+        }
+        else {
+            cout << "Option not available\n";
         }
     }
 
