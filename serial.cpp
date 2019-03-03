9c43cb0f7731bdaeb82ba504f4b7849bb2f90291
Daniele Perot <perotdaniele@gmail.com>
2019-03-03 15:57:30 +0100

    added float to double conversion in serial.cpp

--- serial.cpp
+++ serial.cpp
@@ -1,11 +1,21 @@
 #include "ArduinoConnect.h"
 #include "FlightGear.h"
 
+typedef struct {
+    int magnetos = 0;
+    float mixture = 0.0;
+    float throttle = 0.0;
+    float aileron = 0.0;
+    float elevator = 0.0;
+    float rudder = 0.0;
+} ARDUINO_Data;
+
 int main()
 {
     unsigned int n;
     char buf[6], key;
-    FGFS_Data fromArduino;
+    ARDUINO_Data fromArduino;
+    FGFS_Data convertData;
     ArduinoConnect *arduino = new ArduinoConnect();
     FlightGear *flightgear = new FlightGear(10540);
 
@@ -15,7 +25,13 @@ int main()
 
         arduino->sendData("0", 1);
         n = arduino->readData(&fromArduino, sizeof(fromArduino));
-        flightgear->setFullData(fromArduino)->send();
+        convertData.aileron = (double) fromArduino.aileron;
+        convertData.elevator = (double) fromArduino.elevator;
+        convertData.rudder = (double) fromArduino.rudder;
+        convertData.mixture = (double) fromArduino.mixture;
+        convertData.throttle = (double) fromArduino.throttle;
+        convertData.magnetos = fromArduino.magnetos;
+        flightgear->setFullData(convertData)->send();
         printf(
             "Aileron: %f, Elevator: %f, Rudder: %f, Mixture: %f, Throttle: %f, Magnetos: %d\n",
             fromArduino.aileron,
@@ -25,7 +41,6 @@ int main()
             fromArduino.throttle,
             fromArduino.magnetos
         );
-
     }
 
     return 0;
